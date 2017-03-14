ff0de1f8004d23aa76f193d64ce0cb5e6f04cd4d
kevincrafts <kevin.crafts@colorado.edu>
2017-03-14 09:33:27 -0600

    cleaning up directory structure

--- modules/custom/cu_timeline/templates/bean--timeline--vertical.tpl.php
+++
@@ -1,7 +0,0 @@
-<section class="cd-vertical-timeline">
-	<div class="events-content">
-		<ol>
-			<?php print render($content['timeline_items']); ?>
-		</ol>
-	</div> <!-- .events-content -->
-</section>
